b81bcce468098aa0a4dc61a6a59e290af97beb8f
Dariia Sliusar <dariiasliusar@gmail.com>
2023-12-22 12:26:29 +0100

    finishes fetch

--- 33/script.js
+++ 33/script.js
@@ -1,4 +1,4 @@
-// fetch("https://jsonplaceholder.typicode.com/posts")
+// fetch(`https://jsonplaceholder.typicode.com/posts`)
 //   .then((response) => {
 //     return response.json();
 //   })
@@ -7,18 +7,18 @@
 
 function getPost(id) {
   return new Promise((resolve, reject) => {
-    fetch("https://jsonplaceholder.typicode.com/posts/${id}")
-      .then((response) => response.json())
-      .then((post) => resolve(post))
-      .catch((err) => reject(err));
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then(response => response.json())
+      .then(post => resolve(post))
+      .catch(err => reject(err));
   });
 }
 // getPost(1).then((post) => console.log(post));
 
 function getPost2(id) {
   const [userType, userId] = id.splip("-");
-  return fetch("https://jsonplaceholder.typicode.com/posts/${id}").then(
-    (response) => response.json()
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then(
+    response => response.json()
   );
 }
 // getPost2("user - 1")
@@ -28,12 +28,12 @@ function getPost2(id) {
 function getPost3(id) {
   return Promise.resolve().then(() => {
     const [userType, userId] = id.splip("-");
-    return fetch("https://jsonplaceholder.typicode.com/posts/${id}").then(
-      (response) => response.json()
+    return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then(
+      response => response.json()
     );
   });
 }
 
 getPost3("user-1")
-  .then((post) => console.log(post))
-  .catch((err) => console.log(err));
+  .then(post => console.log(post))
+  .catch(err => console.log(err));
